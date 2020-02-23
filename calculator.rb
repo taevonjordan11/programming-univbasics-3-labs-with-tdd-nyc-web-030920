2b2d868a4141ea280ded26c1c2eeed091ada2875
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-23 20:03:53 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 first_number = 2
 second_number = 3
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quoteint = first_number / second_number
